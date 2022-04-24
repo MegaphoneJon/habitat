13bf9f93d0b3459bee9857fc361731cc0254e1eb
Jon Goldberg <jon@megaphonetech.com>
2022-04-24 17:02:09 -0400

    reduce 'Extensions check disabled' to INFO level

--- habitat.php
+++ habitat.php
@@ -23,6 +23,10 @@ function habitat_modifyChecks($event) {
     if ($env === 'Development' && $name === 'checkDebug') {
       unset($event->messages[$key]);
     }
+    // Reduce 'Extensions check disabled' notice to info
+    if ($name === 'checkExtensions' && $message->getTitle() === E::ts('Extensions check disabled')) {
+      $event->messages[$key]->setLevel(\Psr\Log\LogLevel::INFO);
+    }
   }
 }
 
@@ -195,31 +199,3 @@ function habitat_civicrm_entityTypes(&$entityTypes) {
 function habitat_civicrm_themes(&$themes) {
   _habitat_civix_civicrm_themes($themes);
 }
-
-// --- Functions below this ship commented out. Uncomment as required. ---
-
-/**
- * Implements hook_civicrm_preProcess().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
- *
-function habitat_civicrm_preProcess($formName, &$form) {
-
-} // */
-
-/**
- * Implements hook_civicrm_navigationMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
- *
-function habitat_civicrm_navigationMenu(&$menu) {
-  _habitat_civix_insert_navigation_menu($menu, 'Mailings', array(
-    'label' => E::ts('New subliminal message'),
-    'name' => 'mailing_subliminal_message',
-    'url' => 'civicrm/mailing/subliminal',
-    'permission' => 'access CiviMail',
-    'operator' => 'OR',
-    'separator' => 0,
-  ));
-  _habitat_civix_navigationMenu($menu);
-} // */
