2c7b171a1d5b01b1070093ed551794ff08f13caf
Jon Goldberg <jon@megaphonetech.com>
2023-12-15 13:45:38 -0500

    habitat civix upgrade for Smarty3

--- habitat.php
+++ habitat.php
@@ -84,24 +84,6 @@ function habitat_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function habitat_civicrm_postInstall() {
-  _habitat_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function habitat_civicrm_uninstall() {
-  _habitat_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
@@ -109,33 +91,3 @@ function habitat_civicrm_uninstall() {
 function habitat_civicrm_enable() {
   _habitat_civix_civicrm_enable();
 }
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function habitat_civicrm_disable() {
-  _habitat_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function habitat_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _habitat_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function habitat_civicrm_entityTypes(&$entityTypes) {
-  _habitat_civix_civicrm_entityTypes($entityTypes);
-}
-
